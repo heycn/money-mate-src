3de15e160c169f28935fd5a44b4d462c6badf86e
heycn <heycn@foxmail.com>
2023-04-10 00:15:25 +0800

    创建 404 错误处理页面

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { ErrorPage } from './components/ErrorPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>root</div>,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/1',
+    element: <div>1</div>
+  }
+])
 
 const div = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(div)
-root.render(<App />)
+
+root.render(<RouterProvider router={router} />)
