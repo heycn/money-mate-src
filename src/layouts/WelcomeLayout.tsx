81142497b9ff69b7146f764eb2ac43f8e5c746bc
heycn <heycn@foxmail.com>
2023-04-12 22:56:54 +0800

    添加过度动画

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,5 +1,25 @@
-import { Outlet } from 'react-router-dom'
+import { animated, useTransition } from '@react-spring/web'
+import type { ReactNode } from 'react'
+import { useLocation, useOutlet } from 'react-router-dom'
 
+const map: Record<string, ReactNode> = {}
 export const WelcomeLayout: React.FC = () => {
-  return <div><Outlet /></div>
+  const location = useLocation()
+  const outlet = useOutlet()
+  map[location.pathname] = outlet
+
+  const transitions = useTransition(location.pathname, {
+    form: { transform: 'translateX(100%)' },
+    enter: { transform: 'translateX(0%)' },
+    leave: { transform: 'translateX(-100%)' },
+    config: { duration: 1000 }
+  })
+
+  return transitions((style, pathName) => {
+    return (
+      <animated.div key={pathName} style={style}>
+        {map[pathName]}
+      </animated.div>
+    )
+  })
 }
