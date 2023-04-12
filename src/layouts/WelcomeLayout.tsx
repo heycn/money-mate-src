ae68b620383957affa84695a62daaff27f2dc311
heycn <heycn@foxmail.com>
2023-04-12 23:05:52 +0800

    让第一个页面没有进入动画

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -8,8 +8,9 @@ export const WelcomeLayout: React.FC = () => {
   const outlet = useOutlet()
   map[location.pathname] = outlet
 
+  const firstPath = location.pathname === '/welcome/1'
   const transitions = useTransition(location.pathname, {
-    form: { transform: 'translateX(100%)' },
+    form: { transform: firstPath ? 'translateX(0%)' : 'translateX(100%)' },
     enter: { transform: 'translateX(0%)' },
     leave: { transform: 'translateX(-100%)' },
     config: { duration: 1000 }
