6ad3b11475763be7d715f25a6a2b3c0e28467a13
heycn <heycn@foxmail.com>
2023-04-12 23:47:46 +0800

    引入并配置 Unocss

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
+import 'virtual:uno.css'
 
 const div = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(div)
