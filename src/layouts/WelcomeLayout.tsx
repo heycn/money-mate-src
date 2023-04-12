e1abc40a3aacee91e6b429dc73db23bbb5af8569
heycn <heycn@foxmail.com>
2023-04-12 23:21:16 +0800

    完善路由动画的细节

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,26 +1,25 @@
 import { animated, useTransition } from '@react-spring/web'
 import type { ReactNode } from 'react'
+import { useRef } from 'react'
 import { useLocation, useOutlet } from 'react-router-dom'
 
-const map: Record<string, ReactNode> = {}
 export const WelcomeLayout: React.FC = () => {
+  const map = useRef<Record<string, ReactNode>>({})
   const location = useLocation()
   const outlet = useOutlet()
-  map[location.pathname] = outlet
-
+  map.current[location.pathname] = outlet
   const firstPath = location.pathname === '/welcome/1'
+
   const transitions = useTransition(location.pathname, {
     form: { transform: firstPath ? 'translateX(0%)' : 'translateX(100%)' },
     enter: { transform: 'translateX(0%)' },
     leave: { transform: 'translateX(-100%)' },
-    config: { duration: 1000 }
+    config: { duration: 350 }
   })
 
-  return transitions((style, pathName) => {
-    return (
-      <animated.div key={pathName} style={style}>
-        {map[pathName]}
-      </animated.div>
-    )
-  })
+  return transitions((style, pathName) =>
+    <animated.div key={pathName} style={style}>
+      {map.current[pathName]}
+    </animated.div>
+  )
 }
