e7d3358f1a8683055cddce72c1a7027686e8a829
heycn <heycn@foxmail.com>
2023-04-16 20:54:00 +0800

    完成时间范围选择组件 TimeRangePicker

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -1,16 +1,21 @@
+import { useState } from "react"
 import { AddItemFloatButton } from "../components/AddItemFloatButton"
-import { ItemRangePicker } from "../components/ItemRangePicker"
+import { ItemRangePicker, TimeRange } from "../components/ItemRangePicker"
 import { Topnav } from "../components/Topnav"
 import { ItemsList } from "./ItemsPage/ItemsList"
 import { ItemsSummary } from "./ItemsPage/ItemsSummary"
+import { useTitle } from "../hooks/useTitle"
 
 export const ItemsPage: React.FC = () => {
+  const [currentTimeRange, setCurrentTimeRange] = useState<TimeRange>('thisMonth')
+  useTitle('收支明细')
+
   return (
     <div>
-      <div bg-gradient='to-b from-#addcd4' px-16px>
-        <Topnav />
-        <ItemRangePicker />
-      </div>
+      <header bg-gradient='to-b from-#addcd4' px-16px shadow-neutral-1 shadow-xl>
+        <Topnav title="收支明细" />
+        <ItemRangePicker currentTimeRange={currentTimeRange} onChange={setCurrentTimeRange} />
+      </header>
       <ItemsSummary />
       <ItemsList />
       <AddItemFloatButton />
--- src/shims.d.ts
+++ src/shims.d.ts
@@ -14,6 +14,7 @@ declare module 'react' {
     mt?: string
     rounded?: string
     fixed?: boolean
+    border?: boolean
   }
   interface SVGProps<T> extends SVGAttributes<T>, ClassAttributes<T> {
     w?: string
