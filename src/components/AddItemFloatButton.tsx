35d99a0f66e9ffc554726fb5f36b3f91602c4116
heycn <heycn@foxmail.com>
2023-04-16 13:45:47 +0800

    封装 Icon.tsx 组件

--- src/components/AddItemFloatButton.tsx
+++ src/components/AddItemFloatButton.tsx
@@ -1,10 +1,10 @@
+import { Icon } from "./Icon"
+
 export const AddItemFloatButton: React.FC = () => {
   return (
-    <button w-56px h-56px bg="#5C33BE" rounded="50%"
-      p-4px b-none text-white fixed bottom-16px right-16px>
-      <svg style={{ fill: 'white', width: '1.2em', height: '1.2em' }}>
-        <use xlinkHref='#add'></use>
-      </svg>
+    <button w-56px h-56px rounded="50%" fixed flex justify-center items-center
+      bg="#5C33BE" text-white bottom-16px right-16px p-4px b-none>
+      <Icon name="add" className="w-32px h-32px" />
     </button>
   )
 }
--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,5 +1,14 @@
-export const Icon: React.FC = () => {
+import c from 'classnames'
+import s from './Icon.module.scss'
+
+interface Props {
+  className?: string
+  name: string
+}
+export const Icon: React.FC<Props> = ({ name, className }) => {
   return (
-    <div>icon</div>
+    <svg className={c(className, s.icon)}>
+      <use xlinkHref={`#${name}`}></use>
+    </svg>
   )
 }
