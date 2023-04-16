40b0dd8659209fe21d3de632eec58280b186a02f
heycn <heycn@foxmail.com>
2023-04-16 17:41:13 +0800

    完成Topnav组件

--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -1,7 +1,13 @@
-import * as React from 'react'
+import { Icon } from "./Icon"
 
-export const Topnav: React.FC = () => {
+interface Props {
+  title?: string
+}
+export const Topnav: React.FC<Props> = ({ title = 'MoneyMate' }) => {
   return (
-    <div>Topnav</div>
+    <div text='#73b19f' flex items-center py-16px>
+      <Icon name="menu" className="color-#73b19f h-24px w-24px" />
+      <h1 text-18px font-700 w='100%' mr-24px text-center>{title}</h1>
+    </div>
   )
 }
--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -7,7 +7,7 @@ import { ItemsSummary } from "./ItemsPage/ItemsSummary"
 export const ItemsPage: React.FC = () => {
   return (
     <div>
-      <div bg='#71bcbf' bg-gradient='to-b from-#368099'>
+      <div bg-gradient='to-b from-#addcd4' px-16px>
         <Topnav />
         <ItemRangePicker />
       </div>
