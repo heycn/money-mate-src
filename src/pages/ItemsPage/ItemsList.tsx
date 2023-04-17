63fe5ab602b8d4346a66ce128b5c0bce919da115
heycn <heycn@foxmail.com>
2023-04-17 10:25:17 +0800

    微调

--- src/pages/ItemsPage/ItemsList.tsx
+++ src/pages/ItemsPage/ItemsList.tsx
@@ -7,13 +7,12 @@ export const ItemsList: React.FC<Props> = ({ items }) => {
     <div>
       <ol mx-16px>
         {items.map(item =>
-          <li key={item.id} flex px-16px justify-between py-4px
-            bg-white rounded-xl mb-8px items-center>
+          <li key={item.id} flex px-4 justify-between py-1 bg-white rounded-6px mb-8px items-center>
             <div flex>
-              <div text-34px mr-8px>😘</div>
+              <div text-36px mr-8px>😘</div>
               <div flex flex-col justify-center py-8px>
-                <div text='#333' text-15px>旅行</div>
-                <div text='#999' text-13px font-300 pt-4px>2011-01-01</div>
+                <div text='#333'>旅行</div>
+                <div text='#999' text-12px font-300 pt-1>2011-01-01</div>
               </div>
             </div>
             <div text='#181818' text-18px font-500>-999</div>
--- src/pages/ItemsPage/ItemsSummary.tsx
+++ src/pages/ItemsPage/ItemsSummary.tsx
@@ -1,9 +1,9 @@
 export const ItemsSummary: React.FC = () => {
   return (
-    <ol bg='#ffffff' flex justify-between items-center mx-16px my-24px rounded-10px py-20px px-28px
+    <ol bg-white flex justify-between items-center mx-16px my-24px rounded-10px py-5 px-8
       text-center color='#999' shadow-lg shadow-light-7>
       <li>
-        <div color-blue font-600 text-22px>
+        <div color-blue font-500 text-22px>
           <span text-12px pr-2px font-mono>¥</span>
           1000
         </div>
@@ -11,7 +11,7 @@ export const ItemsSummary: React.FC = () => {
       </li>
       <span font-300>|</span>
       <li>
-        <div color-red font-600 text-22px>
+        <div color-red font-500 text-22px>
           <span text-12px pr-2px font-mono>¥</span>
           1000
         </div>
@@ -19,7 +19,7 @@ export const ItemsSummary: React.FC = () => {
       </li>
       <span font-300>|</span>
       <li>
-        <div color='#666' font-600 text-22px>
+        <div color='#666' font-500 text-22px>
           <span text-12px pr-2px font-mono>¥</span>
           1000
         </div>
