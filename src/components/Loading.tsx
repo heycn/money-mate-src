be90edc66df4183ef4414e74ac96233c8604779f
heycn <heycn@foxmail.com>
2023-04-18 23:56:49 +0800

    调整 Loading 样式

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -7,13 +7,16 @@ const unocss = `animate-spin animate-count-infinite transform-origin-center
 
 export const Loading: React.FC<Props> = ({ message }) => {
   return (
-    <div h-screen flex flex-col justify-center items-center>
-      <svg h-112px fill-none viewBox="0 0 120 120" version="1.1" xmlns="http://www.w3.org/2000/svg">
-        <circle className={unocss} strokeDasharray={0.7} animate-reverse cx="60" cy="60" r="20" pathLength="1" />
-        <circle className={unocss} strokeDasharray={0.7} cx="60" cy="60" r="30" pathLength="1" />
-        <circle fill='#76b6a3' cx="60" cy="60" r="10" />
-      </svg>
-      <p color='#76b6a3' mt--2>{message || '正在加载'}</p>
+    <div h-screen flex flex-col justify-center items-center
+      fixed top-0 left-0 w-screen className="bg-black:15">
+      <div text-center className="bg-white:90" px-24px p-b-20px rounded-10px>
+        <svg w-112px fill-none viewBox="0 0 120 120" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <circle className={unocss} strokeDasharray={0.7} animate-reverse cx="60" cy="60" r="20" pathLength="1" />
+          <circle className={unocss} strokeDasharray={0.7} cx="60" cy="60" r="30" pathLength="1" />
+          <circle fill='#76b6a3' cx="60" cy="60" r="10" />
+        </svg>
+        <p color='#76b6a3' mt--12px>{message || '正在加载'}</p>
+      </div>
     </div>
   )
 }
