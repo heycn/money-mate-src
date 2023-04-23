48b3c015674dda0334324bb2aa5b17305e241e83
heycn <heycn@foxmail.com>
2023-04-23 21:22:51 +0800

    可以切换内容

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { ReactNode, useState } from "react"
 import { Gradient } from "../components/Gradient"
 import { Tabs } from "../components/Tabs"
 import { TopNav } from "../components/TopNav"
@@ -6,21 +6,26 @@ import { TopNav } from "../components/TopNav"
 type ItemKind = 'income' | 'expense'
 
 export const ItemsNewPage: React.FC = () => {
-  const tabItems: { key: ItemKind; text: string }[] = [
-    { key: 'expense', text: '支出' },
-    { key: 'income', text: '收入' }
+  const tabItems: { key: ItemKind; text: string, element?: ReactNode }[] = [
+    { key: 'expense', text: '支出', element: <div>支出</div> },
+    { key: 'income', text: '收入', element: <div>收入</div> }
   ]
   const [currentItemKind, setCurrentItemKind] = useState<ItemKind>('expense')
 
   return (
-    <Gradient>
-      <TopNav title="记一笔" icon='back' />
-      <Tabs
-        tabItems={tabItems}
-        value={currentItemKind}
-        onChange={setCurrentItemKind}
-        className="children-flex-1 text-center"
-      />
-    </Gradient>
+    <>
+      <Gradient>
+        <TopNav title="记一笔" icon='back' />
+        <Tabs
+          tabItems={tabItems}
+          value={currentItemKind}
+          onChange={setCurrentItemKind}
+          className="children-flex-1 text-center"
+        />
+      </Gradient>
+      <ol>
+        {tabItems.find(item => item.key === currentItemKind)?.element}
+      </ol>
+    </>
   )
 }
