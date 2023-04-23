88df87f4187fc12a9139645d988df3774f25bdc4
heycn <heycn@foxmail.com>
2023-04-23 23:06:01 +0800

    使用 Grid 进行 Tags 的布局

--- src/pages/ItemsNewPage/Tags.tsx
+++ src/pages/ItemsNewPage/Tags.tsx
@@ -1,15 +1,30 @@
+import { Icon } from "../../components/Icon"
+
 type Props = {
   kind: Item['kind']
 }
 
 export const Tags: React.FC<Props> = () => {
-  const tags = Array.from({ length: 99 })
+  const tags = Array.from({ length: 10 })
 
   return (
-    <div>
-      <ol grid grid-cols="[repeat(auto-fit,48px)]" justify-center gap-x-32px gap-y-16px py-16px px-8px>
-        {tags.map(() => <li w-48px h-48px b-1 b-red flex justify-center items-center>😀</li>)}
-      </ol>
-    </div>
+    <ol grid grid-cols="[repeat(auto-fit,48px)]"
+      justify-center gap-x-32px gap-y-16px py-16px px-8px
+    >
+      <li b-2 b-white bg-white rounded='50%' w-56px h-56px
+        flex justify-center items-center text-20px color='#77B4A2'
+      >
+        <Icon name="add" />
+      </li>
+      {tags.map(() =>
+        <li flex justify-center items-center flex-col>
+          <div b-2 b='#77B4A2' bg-white rounded='50%'
+            w-56px h-56px flex justify-center items-center text-26px
+          >
+            😀
+          </div>
+          <div text-12px text='#666' mt-6px max-w-54px>打车</div>
+        </li>)}
+    </ol>
   )
 }
