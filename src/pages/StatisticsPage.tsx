75ad3fdcdd6ad684eeb8f6978242ee1a6c683d83
heycn <heycn@foxmail.com>
2023-04-26 11:01:25 +0800

    创建 LineChart 组件

--- src/pages/StatisticsPage.tsx
+++ src/pages/StatisticsPage.tsx
@@ -2,12 +2,9 @@ import { useState } from "react"
 import { AddItemFloatButton } from "../components/AddItemFloatButton"
 import { Gradient } from "../components/Gradient"
 import { ItemRangePicker, TimeRange } from "../components/ItemRangePicker"
-import { TopMenu } from "../components/TopMenu"
 import { TopNav } from "../components/TopNav"
 import { useTitle } from "../hooks/useTitle"
-import { useMenuStore } from "../stores/useMenuStore"
-import { ItemsList } from "./ItemsPage/ItemsList"
-import { ItemsSummary } from "./ItemsPage/ItemsSummary"
+import { LineChart } from "../components/LineChart"
 
 type Props = {
   title?: string
@@ -17,13 +14,20 @@ export const StatisticsPage: React.FC<Props> = ({ title }) => {
   const [currentTimeRange, setCurrentTimeRange] = useState<TimeRange>('thisMonth')
   useTitle(title)
 
+  const items = [
+    { date: '2000-01-01', value: 15000 },
+    { date: '2000-01-02', value: 25000 },
+    { date: '2000-01-31', value: 10000 },
+  ].map(item => ({ x: item.date, y: item.value }))
+
   return (
-    <div h-screen>
+    <div>
       <Gradient>
         <TopNav title={title} icon="back" />
         <ItemRangePicker currentTimeRange={currentTimeRange} onChange={setCurrentTimeRange} />
       </Gradient>
       <AddItemFloatButton />
+      <LineChart className="h-400px" items={items} />
     </div>
   )
 }
