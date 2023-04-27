9c723e5aa343b474341eb2b9f007ad110ed47d11
heycn <heycn@foxmail.com>
2023-04-27 10:24:39 +0800

    修改样式

--- uno.config.ts
+++ uno.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     'm-btn': 'py-14px bg-#36b59d rounded-8 w-100% text-16px font-600 text-#f4f4f4',
     'input-sign-in': 'p-12px placeholder-#D8DCE4 color-#333 text-14px grow-1',
     'form-item-sing-in': 'flex items-center b-b-1 b-#D2DBE3',
-    'send-code': 'bg-light b-#2fac92 b-1 rounded-8 py-6px px-3 color-#2fac92'
+    'send-code': 'bg-light b-#2fac92 b-1 w-98px rounded-8 py-6px color-#2fac92'
   },
   safelist: [],
   rules: [
