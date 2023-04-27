0c682027cb40108ff20f8ea486611f4e2f88aab0
heycn <heycn@foxmail.com>
2023-04-27 13:05:31 +0800

    封装 useCountDown

--- src/components/Input/SmsCodeInput.tsx
+++ src/components/Input/SmsCodeInput.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from "react"
 import { Icon } from "../Icon"
+import { useCountDown } from "../../hooks/useCountDown"
 
 type Props = {
   value?: string
@@ -8,45 +9,22 @@ type Props = {
   request?: () => Promise<unknown>
 }
 const codeConfig = { type: 'text', maxLength: 6, autoComplete: "off", placeholder: '输入验证码' }
-const maxCount = 60
 
 export const SmsCodeInput: React.FC<Props> = ({ value, onChange, request }) => {
-  const [started, setStarted] = useState<Date>()
-  const [count, setCount] = useState(maxCount)
-  const timer = useRef<number>()
+  const [seconds, setSeconds] = useCountDown(0)
 
-  useEffect(() => {
-    if (!started) {
-      clearTimer()
-      return
-    }
-    timer.current = window.setInterval(() => {
-      const seconds = Math.round((new Date().getTime() - started.getTime()) / 1000)
-      const count = maxCount - seconds
-      if (count < 0) { setStarted(void 0) }
-      setCount(count)
-    }, 1000)
-    return clearTimer()
-  }, [started])
-
-  const clearTimer = () => {
-    if (timer.current) {
-      window.clearInterval(timer.current)
-      timer.current = undefined
-    }
-  }
   const onClick = async () => {
-    if (!request) { return }
-    await request()
-    setStarted(new Date())
+    // if (!request) { return }
+    // await request()
+    setSeconds(3)
   }
 
   return (
     <div form-item-sing-in>
       <Icon className='w-24px h-24px' name='menu' />
       <input {...codeConfig} input-sign-in value={value} onChange={e => onChange?.(e.target.value)} />
-      <button type="button" send-code onClick={onClick} disabled={!!started}>
-        {started ? `重新发送 ${count}s` : '发送验证码'}
+      <button type="button" send-code onClick={onClick} disabled={seconds !== 0}>
+        {seconds > 0 ? `重新发送 ${seconds}s` : '发送验证码'}
       </button>
     </div>
   )
---
+++ src/hooks/useCountDown.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useRef, useState } from 'react'
+
+type CountDown = (second: number) => [number, (seconds: number) => void]
+export const useCountDown: CountDown = second => {
+  const [seconds, setSeconds] = useState(second)
+  const timer = useRef<number>()
+
+  useEffect(() => {
+    if (seconds > 0) {
+      timer.current = window.setInterval(() => {
+        console.log('update')
+        setSeconds(seconds - 1)
+      }, 1000)
+    }
+    return () => window.clearInterval(timer.current)
+  }, [seconds])
+
+  return [seconds, setSeconds]
+}
