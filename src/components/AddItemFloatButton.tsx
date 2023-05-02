c20230bb22e59f429d38db603f29ccf8a70a5df8
heycn <heycn@foxmail.com>
2023-05-02 16:25:56 +0800

    使用 preload 在 Router 的 loader 里发请求，利用 swr 的缓存减少不必要的请求

--- src/components/AddItemFloatButton.tsx
+++ src/components/AddItemFloatButton.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom'
 import { Icon } from "./Icon"
 
 export const AddItemFloatButton: React.FC = () => {
   return (
-    <button w-48px h-48px rounded="50%" fixed flex justify-center items-center
-      bg='#5eb39e' text='#f4f4f4' bottom-24px right-24px p-4px b-none>
-      <Icon name="add" className="w-24px h-24px" />
-    </button>
+    <Link to="/items/new" replace>
+      <button w-48px h-48px rounded="50%" fixed flex justify-center items-center
+        bg='#5eb39e' text='#f4f4f4' bottom-24px right-24px p-4px b-none>
+        <Icon name="add" className="w-24px h-24px" />
+      </button>
+    </Link>
   )
 }
